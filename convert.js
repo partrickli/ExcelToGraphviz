5b07e39b117eca24b05e3a10356ded030989f7ec
李桂炎 <partrick.li@gmail.com>
2018-11-06 16:13:19 +0800

    refactor: Extract link reading from excel as function

--- convert.js
+++ convert.js
@@ -3,6 +3,26 @@ const fs = require('fs');
 const path = require('path');
 
 /**
+ * @description Read link from excel sheet, and filter data not available
+ */
+function readLinks() {
+  // read output graphviz dot file path from nodejs argument
+  const inputFileName = process.argv[2];
+  const inputPath = path.join(__dirname, inputFileName);
+
+  const read = xlsx.readFile(inputPath);
+  const accessSheet = 'links';
+  const sheet = read.Sheets[accessSheet];
+  const links = xlsx.utils.sheet_to_json(sheet);
+
+  // filter blank lines of link
+  const filteredLinks = links.filter((link) => {
+    return link['start'] != undefined && link['end'] != undefined;
+  });
+  return filteredLinks;
+}
+
+/**
  * draw a link between two node in a row of sheet
  */
 function drawLink(link) {
@@ -25,20 +45,7 @@ function graph(links) {
 }
 
 // Read links from excel
-const outputFileName = 'gv.dot';
-const inputFileName = process.argv[2];
-const inputPath = path.join(__dirname, inputFileName);
-
-const read = xlsx.readFile(inputPath);
-
-const accessSheet = 'links';
-
-const sheet = read.Sheets[accessSheet];
-
-const links = xlsx.utils.sheet_to_json(sheet);
-const filteredLinks = links.filter((link) => {
-  return link['start'] != undefined && link['end'] != undefined;
-});
+const filteredLinks = readLinks();
 
 // Graphviz command
 const gv = graph(filteredLinks);
